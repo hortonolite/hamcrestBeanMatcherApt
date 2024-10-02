b1059bbbccc629a022dfd7639a1c95182f703204
foal <stas@jresearch.org>
2024-10-02 13:16:26 +0200

    Allows redefine attribute matchers

--- annotation/src/main/java/org/jresearch/hamcrest/beanmatcher/matcher/AbstractBeanMatcher.java
+++ annotation/src/main/java/org/jresearch/hamcrest/beanmatcher/matcher/AbstractBeanMatcher.java
@@ -1,34 +1,34 @@
 package org.jresearch.hamcrest.beanmatcher.matcher;
 
-import static org.hamcrest.beans.HasPropertyWithValue.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import one.util.streamex.StreamEx;
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 
-import one.util.streamex.StreamEx;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.beans.HasPropertyWithValue.*;
 
 public class AbstractBeanMatcher<T> extends BaseMatcher<T> {
 
-	private final List<Matcher<? super T>> propertyMatchers = new ArrayList<>();
+	private final Map<String, Matcher<? super T>> propertyMatchers = new HashMap<>();
 
 	private List<Matcher<? super T>> failedMatchers = new ArrayList<>();
 
 	protected void addPropertyMatcher(String propertyName, final Matcher<?> propertyMatcher) {
-		addBeanMatcher(hasProperty(propertyName, propertyMatcher));
+		addBeanMatcher(propertyName, hasProperty(propertyName, propertyMatcher));
 	}
 
-	protected void addBeanMatcher(final Matcher<? super T> matcher) {
-		propertyMatchers.add(matcher);
+	protected void addBeanMatcher(String propertyName, final Matcher<? super T> matcher) {
+		propertyMatchers.put(propertyName, matcher);
 	}
 
-	@SuppressWarnings("resource")
 	@Override
 	public boolean matches(final Object item) {
-		failedMatchers = StreamEx.of(propertyMatchers).remove(m -> m.matches(item)).toList();
+		failedMatchers = StreamEx.of(propertyMatchers.values()).remove(m -> m.matches(item)).toList();
 		return failedMatchers.isEmpty();
 	}
 
